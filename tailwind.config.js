ca5c672c09e916d8feb420296e52d5552a8de49e
Nick Been <66799549+nickbeen@users.noreply.github.com>
2022-03-22 20:37:36 +0100

    Implemented tailwind css for generating css utility classes

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './_includes/*.html',
+    './_layouts/*.html',
+    './_posts/*.md',
+    './assets/js/*.js',
+    './*.{html,md}'
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
